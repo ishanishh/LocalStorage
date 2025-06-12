b610ec4919f3ce108988d52546da3869c352f8de
ishan shrestha <ishanstha530@gmail.com>
2025-06-12 09:14:14 +0545

    added an email field

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function store(){
-    var name,hex,contact;
+    var name,hex,contact,email;
 
 name = document.getElementById("Pname").value;
 hex = document.getElementById("Hcode").value;
 contact = document.getElementById("PhNo").value;
+e = document.getElementById("Email").value;
 
 var pname = localStorage.setItem("paint name", name);
 var hexa = localStorage.setItem("hex code", hex);
 var contact = localStorage.setItem("contact", contact);
+var email = localStorage.setItem("email", e);
 }
