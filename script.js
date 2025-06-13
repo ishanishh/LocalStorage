888994a0a3dbb328c1752a274ad9fea94fb4abfe
ishan shrestha <ishanstha530@gmail.com>
2025-06-13 20:47:51 +0545

    first and last name added

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function store(){
-    var name,hex,contact,email;
+    var name,hex,contact,email,first, last;
 
 name = document.getElementById("Pname").value;
 hex = document.getElementById("Hcode").value;
 contact = document.getElementById("PhNo").value;
 e = document.getElementById("Email").value;
+first = document.getElementById("Fname").value;
+last = document.getElementById("Lname").value;
 
 var pname = localStorage.setItem("paint name", name);
 var hexa = localStorage.setItem("hex code", hex);
 var contact = localStorage.setItem("contact", contact);
 var email = localStorage.setItem("email", e);
+var fname = localStorage.setItem("firstname", first);
+var lname = localStorage.setItem("last name", last);
 }
+
